d463b1bda5c9b8c1a29699e115bb769b48bd957d
DarkBall123 <124524424+DarkBall123@users.noreply.github.com>
2023-08-18 18:22:52 +0300

    Add files via upload

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 2999530018;
+name = "Lancet  loitering munition";
+timestamp = 5249957823358251337;
---
+++ mod.cpp
@@ -0,0 +1,12 @@
+name = "Lancet";
+tooltipOwned = "Tool Tip";
+overview = "Lancet";
+
+picture     = "data\1024.paa"; 
+logoSmall	= "data\128.paa";	// Display next to the item added by the mod
+logo		= "data\256.paa";			// Logo displayed in the main menu
+
+action = "";
+
+hideName = 0;
+hidePicture = 0;
